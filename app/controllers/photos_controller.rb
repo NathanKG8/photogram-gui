8c63566cac7cef6a6d44fa12bd6f3a6230ab7e1e
NathanKG8 <164825742+NathanKG8@users.noreply.github.com>
2024-05-08 05:05:44 +0000

    Progress

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -28,4 +28,26 @@ class PhotosController < ApplicationController
     a_new_photo.save
     redirect_to("/photos/" + a_new_photo.id.to_s)
   end
+  def update
+    the_id = params.fetch("path_id")
+    matching_photos = Photo.where({:id => the_id})
+    the_photo = matching_photos.at(0)
+    input_image = params.fetch("input_image")
+    input_caption = params.fetch("input_caption")
+    the_photo.image = input_image
+    the_photo.caption = input_caption
+    the_photo.save
+    redirect_to("/photos/" + the_photo.id.to_s)
+  end
+  def comment
+    input_photo_id = params.fetch("input_photo_id")
+    input_author_id = params.fetch("input_author_id")
+    input_comment = params.fetch("input_comment")
+    a_new_comment = Comment.new
+    a_new_comment.photo_id = input_photo_id
+    a_new_comment.author_id = input_author_id
+    a_new_comment.body = input_comment
+    a_new_photo.save
+    redirect_to("/photos/" + a_new_comment.photo_id.to_s)
+  end
 end
